1d59c241a40cb2492d30e8bf474ea325e62374a8
Brian Lauber <blauber@arclia.com>
2024-01-25 22:50:52 -0500

    Revenue Chart sorta works now

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,26 +10,30 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+import { withFakeClient } from './fake-vercel';
+
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
-  try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data.rows;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
-  }
+  return await withFakeClient(async function(client) {
+    try {
+      // Artificially delay a response for demo purposes.
+      // Don't do this in production :)
+  
+      // console.log('Fetching revenue data...');
+      // await new Promise((resolve) => setTimeout(resolve, 3000));
+  
+      const data = await client.sql<Revenue>`SELECT * FROM revenue`;
+  
+      // console.log('Data fetch completed after 3 seconds.');
+  
+      return data.rows;
+    } catch (error) {
+      console.error('Database Error:', error);
+      throw new Error('Failed to fetch revenue data.');
+    }
+  });
 }
 
 export async function fetchLatestInvoices() {
